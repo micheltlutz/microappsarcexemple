c7ac45e3a4affd8692ec92cd6a174e22c943f03e
Michel Anderson Lutz Teixeira <michel.lutz@compasso.com.br>
2022-11-07 16:30:39 -0300

    Run tests

---
+++ MicroappArcExampleTests/MicroappArcExampleTests.swift
@@ -0,0 +1,15 @@
+//
+//  MicroappArcExampleTests.swift
+//  MicroappArcExampleTests
+//
+//  Created by Michel Anderson Lutz Teixeira on 07/11/22.
+//
+
+import XCTest
+
+final class MicroappArcExampleTests: XCTestCase {
+    func testExample() throws {
+        let fakeTest = false
+        XCTAssertTrue(fakeTest)
+    }
+}
